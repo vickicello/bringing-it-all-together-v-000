861a4be1d533278df00c54153ee1f908845593b5
Vicki Hamm <vicki.cello@gmail.com>
2018-08-07 20:49:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,6 +23,21 @@ class Dog
       sql = "DROP TABLE IF EXISTS dogs"
       DB[:conn].execute(sql)
     end
+
+    def save
+      if self.id
+        self.update
+      else
+        sql = <<-SQL
+          INSERT INTO dogs (name, breed)
+          VALUES (?, ?)
+       SQL
+       DB[:conn].execute(sql, self.name, self.breed)
+       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+     end
+    end
+
+    
   # def self.find(id, db)
   #    pokemon_data = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
   #    self.new(id: pokemon_data[0], name: pokemon_data[1], type: pokemon_data[2], db: db, hp: pokemon_data[3] )
