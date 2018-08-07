d750ced1180fe23878272ca1bb640cacf5a15162
Vicki Hamm <vicki.cello@gmail.com>
2018-08-07 20:43:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,18 @@
 class Dog
 
+  attr_accessor :name, :breed
+  attr_reader :id
+
   def initialize(id:, name:, breed:)
+    self.id = id
+    self.name = name
+    self.breed = breed
   end
 
+  # def self.find(id, db)
+  #    pokemon_data = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+  #    self.new(id: pokemon_data[0], name: pokemon_data[1], type: pokemon_data[2], db: db, hp: pokemon_data[3] )
+  #  end
 
 
 end
