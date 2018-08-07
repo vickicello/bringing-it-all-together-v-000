61b599d6ee93a3ffc1ece59d2ed48434a5f74a0e
Vicki Hamm <vicki.cello@gmail.com>
2018-08-07 20:48:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,22 @@ class Dog
     self.name = name
     self.breed = breed
   end
+  
+    def self.create_table
+      sql =  <<-SQL
+        CREATE TABLE IF NOT EXISTS dogs (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          breed TEXT
+          )
+          SQL
+      DB[:conn].execute(sql)
+    end
 
+    def self.drop_table
+      sql = "DROP TABLE IF EXISTS dogs"
+      DB[:conn].execute(sql)
+    end
   # def self.find(id, db)
   #    pokemon_data = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
   #    self.new(id: pokemon_data[0], name: pokemon_data[1], type: pokemon_data[2], db: db, hp: pokemon_data[3] )
